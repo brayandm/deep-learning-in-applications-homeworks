9aa97df31ee8b88c0a4d8d3351206a058f5eda50
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-04-12 13:26:13 +0200

    Update dependencies in install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pip3 install numpy pandas matplotlib scikit-learn scikit-plot scipy seaborn ipykernel xgboost mlxtend tqdm
+pip3 install numpy pandas matplotlib scikit-learn scikit-plot scipy==1.12 seaborn ipykernel xgboost mlxtend tqdm
 
 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
 
