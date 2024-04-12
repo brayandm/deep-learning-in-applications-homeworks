6694d41f4d3161a539ba055bb88b58c5ef554132
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-04-12 13:25:30 +0200

    Add installation script for required Python packages

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+pip3 install numpy pandas matplotlib scikit-learn scikit-plot scipy seaborn ipykernel xgboost mlxtend tqdm
+
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+
+pip3 install gensim nltk
